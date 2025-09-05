ce5376e67ea9f72bd261f4ca3678c285d9456d22
Eric DONYOH <39004197+DONYOH@users.noreply.github.com>
2025-09-05 11:45:51 +0200

    Create code.py
    code pour faire le nettoyage

---
+++ code.py
@@ -0,0 +1,5 @@
+import pandas as pd
+df = pd.read_csv("Lien vers le fichier")
+df.drop_duplicates(inplace=True)
+df.fillna("Non renseigné", inplace=True)
+df['date'] = pd.to_datetime(df['date'], errors='coerce')
